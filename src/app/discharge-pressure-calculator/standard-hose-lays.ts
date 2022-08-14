fad400d8783779e8ee01f03b1b42ce3e1039dd5c
unknown <along8@umbc.edu>
2022-08-14 17:50:11 -0400

    add deck gun

--- src/app/discharge-pressure-calculator/standard-hose-lays.ts
+++ src/app/discharge-pressure-calculator/standard-hose-lays.ts
@@ -3,7 +3,7 @@ import { HoseLay } from "./types";
 export const STANDARD_HOSE_LAYS: HoseLay[] = [
     {
         name: "Front Jumpline",
-        length: 150,
+        length: 75,
         diameter: 2,
         nozzlePressure: 100,
         gpm: 100,
@@ -32,5 +32,13 @@ export const STANDARD_HOSE_LAYS: HoseLay[] = [
         nozzlePressure: 100,
         gpm: 100,
         displayColor: 'blue'
+    },
+    {
+        name: "Deck Gun",
+        length: 0,
+        diameter: 1,
+        nozzlePressure: 125,
+        gpm: 100,
+        displayColor: 'silver'
     },
 ];
